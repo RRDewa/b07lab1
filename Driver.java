753d228ae0767b46bf1e95d69a3b6730673350fd
Rizky Rajendra Ananta Dewa <rranantadewa@gmail.com>
2024-06-02 21:12:12 -0400

    “modified Polynomial.java and Driver.java”

--- Driver.java
+++ Driver.java
@@ -1,16 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+    public static void main(String[] args){
+        Polynomial p0 = new Polynomial();
+        System.out.println(p0.max_deg());
+        double[] p1_coef = {1,2,3};
+        int[] p1_expo = {0,3,2};
+        Polynomial p1 = new Polynomial(p1_coef, p1_expo);
+        double[] p2_coef = {-1,-2,-3};
+        int[] p2_expo = {0,3,2};
+        Polynomial p2 = new Polynomial(p2_coef, p2_expo);
+        System.out.println(p2.max_deg());
+        Polynomial p12_sum = p1.add(p2);
+        System.out.println(p12_sum.max_deg());
+        Polynomial p12_mult = p1.multiply(p2);
+        System.out.println((p12_mult.expo)[4]);
+        Polynomial p10_mult = p1.multiply(p0);
+        System.out.println(p10_mult.max_deg());
+        System.out.println(p12_mult.evaluate(3));
+
+        double[] c1_coef = {6,5};
+        int[] c1_expo = {0,3};
+        Polynomial c1 = new Polynomial(c1_coef, c1_expo);
+
+        double[] c2_coef = {-2,-9};
+        int[] c2_expo = {1,4};
+        Polynomial c2 = new Polynomial(c2_coef, c2_expo);
+
+        Polynomial s = c1.add(c2);
+        if(s.hasRoot(1)){
+            System.out.println("1 is a root of s");
+        }
+        else{
+            System.out.println("1 is not a root of s");
+        }
+
+
+        Path cur = Paths.get("filepoly2.txt");
+            String ss = cur.toAbsolutePath().toString();
+            Path p = Paths.get(ss);
+            try{
+                Files.writeString(p, "1.0x1", StandardCharsets.UTF_8);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+        }
+        c2.saveToFile("filepoly1.txt");
+        File mypoly = new File("filepoly2.txt"); 
+        Polynomial c1file = new Polynomial(mypoly);
+        System.out.println(c1file.max_deg());
+        System.out.println((c1file.expo)[0]);
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,71 +1,249 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
 public class Polynomial{
 	//Fields
-	double [] poly; 
+	double [] coef; 
+	int [] expo;
 	
 	//Methods
 	public Polynomial(){
-		poly = new double [1];
-		poly[0] = 0;
+		coef = null;
+		
+		expo = null ;
 	}
-	public Polynomial(double [] px){
-		this.poly = px;
+	public Polynomial(double [] coef, int [] expo){
+		this.coef = coef;
+		this.expo = expo;
+	}
+	public Polynomial(File textfile){
+		String purestring = textfile.getAbsolutePath();
+		String poly_s = "";
+		try{
+			poly_s = new String(Files.readAllBytes(Paths.get(purestring)));
+		}
+		catch(IOException e){
+	        e.printStackTrace();
+	    }
+		String[] arr_spoly = poly_s.split("[+-]");
+		int len_arr = arr_spoly.length;
+		coef = new double[len_arr];
+		expo = new int[len_arr];
+
+		for(int i = 0; i < len_arr ; i = i + 1){
+			int idxx = arr_spoly[i].indexOf('x');
+			if (idxx < 0){
+				coef[i] = Double.parseDouble(arr_spoly[i]);
+				expo[i] = 0;
+			}
+			else{
+				coef[i] = Double.parseDouble(arr_spoly[i].substring(0, idxx - 1));
+				expo[i] = Integer.parseInt(arr_spoly[i].substring(idxx + 1));
+
+			}
+		}
+		if(poly_s.charAt(0) == '-') {
+			coef[0] = coef[0] * -1;
+		}
+		int iter = 1;
+		for(int i = 1; i < poly_s.length(); i = i + 1){
+			if(poly_s.charAt(i) == '-'){
+				coef[iter] *= coef[iter] * -1;
+				iter = iter + 1;
+			}
+			else if(poly_s.charAt(i) == '+'){
+				iter = iter + 1;
+			}
+		}
+
+	}
+
+	public int max_deg(){
+		if (this.expo == null){
+			return -1;
+		}
+		int l = (this.expo).length;
+		int max_deg = (this.expo)[0];
+		for (int i = 0 ; i < l ; i = i + 1){
+			if (max_deg < (this.expo)[i]) {
+				max_deg = (this.expo)[i];
+			}
+		}
+		return max_deg;
 	}
 	public Polynomial add( Polynomial p1){
-		int l1 = (p1.poly).length;
-		int l = (this.poly).length;
-		int min_len;
-		int max_len;
-		if (l1 <= l){
-			min_len = l1;
-			max_len = l;
+
+		if (p1.expo == null){
+            Polynomial res = new Polynomial();
+            res.coef = p1.coef;
+            res.expo = p1.expo;
+            return res;
+        }
+
+        if (this.expo == null){
+            Polynomial res = new Polynomial();
+            res.coef = coef;
+            res.expo = expo;
+            return res;
+        }
+
+		int l1 = (p1.expo).length;
+		int l = (this.expo).length;
+
+        int max_deg;
+		if (p1.max_deg() > this.max_deg()){
+			max_deg = p1.max_deg();
 		}
-		else{
-			min_len = l;
-			max_len = l1;
+		else {
+			max_deg = this.max_deg();
 		}
-		double [] poly_res = new double[max_len];
-		
-		int idx = 0;
-		for (int i = 0; i < min_len ; i = i + 1){
-			poly_res[i] = (p1.poly)[i] + poly[i];
-			idx = i + 1;
+
+		double [] coef_iter = new double[max_deg + 1];
+		for (int i = 0 ; i < (max_deg + 1) ; i = i + 1){
+			coef_iter[i] = 0;
 		}
-		if(l1 < l){
-			for (int i = idx; i < max_len ; i = i +1){
-				poly_res[i] = poly[i];
+		// int [] expo_iter = new int[max_deg + 1];
+		int deg_iter;
+
+		for (int i = 0 ; i < l ; i = i + 1){
+			deg_iter = (this.expo)[i];
+			coef_iter[deg_iter] = coef_iter[deg_iter] + (this.coef)[i];
+		}
+
+		for (int i = 0 ; i < l1 ; i = i + 1){
+			deg_iter = (p1.expo)[i];
+			coef_iter[deg_iter] = coef_iter[deg_iter] + (p1.coef)[i];
+		}
+
+		int natural_deg = 0;
+		for (int i = 0 ; i < (max_deg + 1) ; i = i + 1){
+			if (coef_iter[i] != 0){
+				natural_deg += 1;
 			}
 		}
-		else{
-			for (int i = idx; i< max_len ; i = i +1){
-				poly_res[i] = (p1.poly)[i];
+
+		if (natural_deg == 0) {
+			Polynomial poly_res = new Polynomial();
+			return poly_res;
+		}
+
+		double[] coef_res = new double[natural_deg];
+		int [] expo_res = new int[natural_deg];
+
+		int expo_iter = 0;
+		for (int i = 0 ; i < (max_deg + 1) ; i = i + 1){
+			if (coef_iter[i] != 0){
+				coef_res[expo_iter] = coef_iter[i];
+				expo_res[expo_iter] = i;
+				expo_iter += 1;
 			}
 		}
-		Polynomial result = new Polynomial(poly_res);
+
+		Polynomial poly_res = new Polynomial (coef_res, expo_res);
+		return poly_res;
+	}
+	public Polynomial multiply(Polynomial p1){
+		if ( (this.expo == null) || (p1.expo == null)){
+            Polynomial res = new Polynomial();
+            return res;
+        }
+
+		int l1 = (p1.expo).length;
+		int l = (this.expo).length;
+
+        int max_deg = (this.max_deg() + p1.max_deg());
+		double[] coef_iter = new double[max_deg + 1];
+
+		int p_expo ;
+		double p_coef ;
+		int p1_expo;
+		double p1_coef;
+		for (int i = 0 ; i < l ;i = i + 1){
+			p_expo = (this.expo)[i];
+			p_coef = (this.coef)[i];
+			for (int j = 0 ; j < l1 ; j = j + 1){
+				p1_expo = (p1.expo)[j];
+				p1_coef = (p1.coef)[j];
+				coef_iter[p_expo + p1_expo] += p_coef*p1_coef;
+			}
+		}
+		int natural_deg = 0;
+		for (int i = 0 ; i < (max_deg + 1) ; i = i + 1){
+			if (coef_iter[i] != 0){
+				natural_deg += 1;
+			}
+		}
+
+		if (natural_deg == 0) {
+			Polynomial poly_res = new Polynomial();
+			return poly_res;
+		}
+
+		double[] coef_res = new double[natural_deg];
+		int [] expo_res = new int[natural_deg];
+
+		int expo_iter = 0;
+		for (int i = 0 ; i < (max_deg + 1) ; i = i + 1){
+			if (coef_iter[i] != 0){
+				coef_res[expo_iter] = coef_iter[i];
+				expo_res[expo_iter] = i;
+				expo_iter += 1;
+			}
+		}
+
+		Polynomial poly_res = new Polynomial (coef_res, expo_res);
+		return poly_res;
+	}
+
+	double evaluate(double arg){
+		if (this.expo == null){
+			return 0;
+		}
+		int max_deg = this.max_deg();
+
+		double result = 0;
+		for (int i = 0 ; i < max_deg ; i = i + 1){
+			double expo_arg = 1;
+			for (int j = 0; j < (this.expo)[i]; j = j + 1){
+				expo_arg = expo_arg*arg;
+			}
+			result = result + (this.coef)[i]*expo_arg;
+		}
 		return result;
 	}
-	public double evaluate(double x){
-		double mapto =0;
-		int idx = 0;
-		for (double coef:poly){
-			if (coef == 0){
-				idx = idx + 1;
-				continue;
-			}
-			double exp = 1;
-			for (int i = 1; i <= idx ; i = i + 1){
-				exp = exp * x;
-			}
-			mapto = mapto + coef * exp;
-			idx = idx + 1;
-		}
-		return mapto;
+	
+	boolean hasRoot(double arg){
+		return (this.evaluate(arg) == 0);
 	}
-	public boolean hasRoot(double x){
-		if (evaluate(x) == 0){
-			return true;
+
+	void saveToFile(String s){
+		String out = "";
+		for(int i = 0; i < coef.length; i++){
+			if( (i != 0) && (coef[i] > 0)){
+				out = out + "+";
+			}
+			out = out + Double.toString(coef[i]);
+			if(expo[i] != 0){
+				out = out + "x";
+				out = out + Integer.toString(expo[i]);
+			}
 		}
-		else{
-			return false;
+		Path cur = Paths.get(s);
+		String ss = cur.toAbsolutePath().toString();
+		Path p = Paths.get(ss);
+		try{
+			Files.writeString(p, out, StandardCharsets.UTF_8);
 		}
+		catch(IOException e){
+	        e.printStackTrace();
+	    }
 	}
+
 }
