7b0d3dfd3b64233748a67b1cd06cad866bb124a8
Rizky Rajendra Ananta Dewa <rranantadewa@gmail.com>
2024-09-17 14:01:36 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,61 +1,100 @@
-
-public class Polynomial {
-	double [] coef ;
+public class Polynomial{
+	//Fields
+	double [] coef; 
+	int [] expo;
+	
+	//Methods
 	public Polynomial(){
-		coef = new double [1];
-		coef[0] = 0;
+		coef = new double [0];
+		
+		expo = new int [0] ;
 	}
-
-	public Polynomial( double [] arr) {
-		int n = arr.length ;
-		coef = new double [n];
-		for(int i = 0; i < n ; i++) {
-			coef[i] = arr[i];
-		}
+	public Polynomial(double [] coef, int [] expo){
+		this.coef = coef;
+		this.expo = expo;
 	}
-	
-	public Polynomial add( Polynomial poly) {
-		int len_call = coef.length;
-		int len_arg = poly.coef.length;
-		if (len_call < len_arg) {
-			double [] p_res = new double[len_arg];
-			int i = 0;
-			for(i = 0 ; i < len_call ; i++) {
-				p_res[i] = poly.coef[i] + coef[i];
-			}
-			while(i<len_arg) {
-				p_res[i] = poly.coef[i];
-				i = i +1;
-			}
-			Polynomial res = new Polynomial(p_res);
-			return res;
+	public Polynomial add( Polynomial p1){
+		int idx = 0;
+		int idx_1 = 0;
+		int l1 = (p1.poly).length;
+		int l = (this.poly).length;
+		
+		int min_len;
+		int max_len;
+		if (l1 <= l){
+			min_len = l1;
+			max_len = l;
 		}
-		else {
-			double [] p_res = new double[len_call];
-			int i = 0;
-			for(i = 0 ; i < len_arg ; i++) {
-				p_res[i] = poly.coef[i] + coef[i];
-			}
-			while(i<len_call) {
-				p_res[i] = coef[i];
-				i = i +1;
-			}
-			Polynomial res = new Polynomial(p_res);
-			return res;
+		else{
+			min_len = l;
+			max_len = l1;
 		}
+		
+		double [] coefx_res = new double[max_len];
+		int [] expox_res = new expo[max_len];
+		
+		int res_len = 0;
+		int idx_iter = 0;
+		int idx1_iter = 0;
+		for (int i = 0; i < (max_len + min_len) ; i = i + 1){
+			if ( ((idx1_iter < l1) && ((p1.expo)[idx1_iter] == i)) ||( (idx_iter < l) && (expo[idx_iter] == i)) ){
+				coefx_res[res_len] = 0;
+				expox_res[res_len] = i;
+				if ((idx1_iter < l1) && ((p1.expo)[idx1_iter] == i)){
+					coefx_res[res_len] += (p1.coef)[idx1_iter];
+					idx1_iter += 1;
+				}
+				if ((idx_iter < l) && (expo[idx_iter] == i)){
+					coefx_res[res_len] += coef[idx_iter];
+					idx_iter += 1;
+				}
+			}
+			res_len += 1;
+		}
+		
+		
+		
+		int idx = 0;
+		for (int i = 0; i < min_len ; i = i + 1){
+			poly_res[i] = (p1.poly)[i] + poly[i];
+			idx = i + 1;
+		}
+		if(l1 < l){
+			for (int i = idx; i < max_len ; i = i +1){
+				poly_res[i] = poly[i];
+			}
+		}
+		else{
+			for (int i = idx; i< max_len ; i = i +1){
+				poly_res[i] = (p1.poly)[i];
+			}
+		}
+		Polynomial result = new Polynomial(poly_res);
+		return result;
 	}
-	public double evaluate( double arg) {
-		double exp = 1;
-		int len_call = coef.length;
-		double res = 0;
-		for(int i = 0 ; i < len_call ; i++) {
-			res += coef[i] * exp;
-			exp = exp * arg;
+	public double evaluate(double x){
+		double mapto =0;
+		int idx = 0;
+		for (double coef:poly){
+			if (coef == 0){
+				idx = idx + 1;
+				continue;
+			}
+			double exp = 1;
+			for (int i = 1; i <= idx ; i = i + 1){
+				exp = exp * x;
+			}
+			mapto = mapto + coef * exp;
+			idx = idx + 1;
 		}
-		return res;
+		return mapto;
 	}
-	
-	public boolean hasRoot( double arg) {
-		return (this.evaluate(arg) == 0);
+	public boolean hasRoot(double x){
+		if (evaluate(x) == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
